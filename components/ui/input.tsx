57fe5b5a3e4dce37a3a300a7c7b78c89e77571bf
LidorTottemai <zakenlidor@gmail.com>
2026-05-25 22:40:02 +0300

    Add components/ui/input.tsx

---
+++ components/ui/input.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import { cn } from './cn';
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={cn(
+          'flex h-11 w-full rounded-lg border border-gray-200 bg-white px-4 py-2 text-sm',
+          'placeholder:text-gray-400',
+          'focus:outline-none focus:ring-2 focus:ring-brand-blue-500 focus:border-transparent',
+          'disabled:cursor-not-allowed disabled:opacity-50',
+          'transition-colors duration-200',
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+Input.displayName = 'Input';
+
+export { Input };
